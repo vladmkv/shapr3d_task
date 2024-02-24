b9485d619162be5e9e8c6506f5999062d009bdf5
Vladimir Malkov <vladimir.malkov@gmail.com>
2024-02-24 16:36:48 +0100

    Basic project structure created

---
+++ geomio/Mesh.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+namespace geomio
+{
+
+class Mesh
+{
+	std::vector<float> buf_;
+};
+
+}
---
+++ geomio/tests/Mesh_test.cpp
@@ -0,0 +1,9 @@
+#include "Mesh.h"
+
+#include <gtest/gtest.h>
+
+TEST(MeshTests, DummyTest)
+{
+    auto mesh = geomio::Mesh{};
+	ASSERT_TRUE(true);
+}
